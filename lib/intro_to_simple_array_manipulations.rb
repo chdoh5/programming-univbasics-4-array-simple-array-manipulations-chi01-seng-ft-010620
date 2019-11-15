9376715c23ec36bfe8a4a4ed12b49833eae30ae9
Christine Doherty <christineadoherty@gmail.com>
2019-11-15 23:18:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,3 +25,12 @@ end
 def using_concat(array_one, array_two)
   array_one.concat(array_two)
 end
+
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_uniq(array)
+  array.uniq 
+end 
+
